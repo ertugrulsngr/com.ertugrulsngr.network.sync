85d3363e88b3ade3753e2ce00ca0927b0de0ad1e
Ertugrul Sungur <82088208+ertugrulsngr@users.noreply.github.com>
2026-08-17 02:27:36 +0300

    chore: pass Vector3/Quaternion by value in AnchoredTransformUtility

--- Runtime/Utility/AnchoredTransformUtility.cs
+++ Runtime/Utility/AnchoredTransformUtility.cs
@@ -5,21 +5,21 @@ namespace NetworkSync.Utility
     public static class AnchoredTransformUtility
     {
         public static Vector3 GetLocalPosition(
-            in Vector3 worldPosition,
-            in Vector3 anchorPosition,
-            in Quaternion anchorRotation,
-            in Vector3 anchorScale)
+            Vector3 worldPosition,
+            Vector3 anchorPosition,
+            Quaternion anchorRotation,
+            Vector3 anchorScale)
         {
             Vector3 unrotated = Quaternion.Inverse(anchorRotation) * (worldPosition - anchorPosition);
             return GetLocalScale(unrotated, anchorScale);
         }
 
-        public static Quaternion GetLocalRotation(in Quaternion worldRotation, in Quaternion anchorRotation)
+        public static Quaternion GetLocalRotation(Quaternion worldRotation, Quaternion anchorRotation)
         {
             return Quaternion.Inverse(anchorRotation) * worldRotation;
         }
 
-        public static Vector3 GetLocalScale(in Vector3 worldScale, in Vector3 anchorScale)
+        public static Vector3 GetLocalScale(Vector3 worldScale, Vector3 anchorScale)
         {
             return new Vector3(
                 anchorScale.x != 0f ? worldScale.x / anchorScale.x : worldScale.x,
@@ -28,20 +28,20 @@ namespace NetworkSync.Utility
         }
 
         public static Vector3 GetWorldPosition(
-            in Vector3 localPosition,
-            in Vector3 anchorPosition,
-            in Quaternion anchorRotation,
-            in Vector3 anchorScale)
+            Vector3 localPosition,
+            Vector3 anchorPosition,
+            Quaternion anchorRotation,
+            Vector3 anchorScale)
         {
             return anchorPosition + (anchorRotation * Vector3.Scale(localPosition, anchorScale));
         }
 
-        public static Quaternion GetWorldRotation(in Quaternion localRotation, in Quaternion anchorRotation)
+        public static Quaternion GetWorldRotation(Quaternion localRotation, Quaternion anchorRotation)
         {
             return anchorRotation * localRotation;
         }
 
-        public static Vector3 GetWorldScale(in Vector3 localScale, in Vector3 anchorScale)
+        public static Vector3 GetWorldScale(Vector3 localScale, Vector3 anchorScale)
         {
             return Vector3.Scale(localScale, anchorScale);
         }
